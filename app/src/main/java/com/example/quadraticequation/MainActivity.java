c9ff69d989d906b5c651d9dd8b25cff03cc10501
ng5134 <ng5134@bs.amalnet.k12.il>
2025-09-28 21:41:09 +0300

    java main

--- app/src/main/java/com/example/quadraticequation/MainActivity.java
+++ app/src/main/java/com/example/quadraticequation/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.quadraticequation;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +12,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import java.util.Random;
 public class MainActivity extends AppCompatActivity {
+    EditText inputA, inputB, inputC;
+    Button btnSolve, btnRandom;
+    TextView lastResult;
 
-
+    String lastSolution="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputA= findViewById(R.id.inputA);
+        inputB= findViewById(R.id.inputB);
+        inputC= findViewById(R.id.inputC);
+        btnSolve= findViewById(R.id.btnSolve);
+        btnRandom= findViewById(R.id.btnRandom);
+        lastResult= findViewById(R.id.LastResult);
+
     }
 }
 
